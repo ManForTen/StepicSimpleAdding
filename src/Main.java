045d48286b8498120d9bf7f5c05a3982efab5c07
Artem Petrikov <manforten@tandex.ru>
2023-02-10 22:00:48 +0300

    New changes

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,37 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
 public class Main {
+    static JFrame frame = new JFrame();//создаем форму
+    static int width = 500, height = 500;
+    static JLayeredPane panel = new JLayeredPane();
+
+    static public void add(MouseEvent e){ // Метод добавления объекта по клику
+        if (e.getButton()==1){ // Если кнопка левая
+            JLabel label = new JLabel("X:"+e.getX()+" Y:"+e.getY());
+            label.setBounds(e.getX(),e.getY(),100,20);
+            panel.add(label);
+        }
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        javax.swing.SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                frame.setTitle("Добавление мышкой");
+                Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
+                frame.setBounds(dim.width / 2 - width / 2, dim.height / 2 - height / 2, width, height);
+                panel.setFocusable(true); // Делаем у панели возможность принимать фокус, иначе она не сможет отловить события клавиатуры
+                frame.add(panel); // Добавляем панель на форму
+                panel.addMouseListener(new MouseAdapter() { // Добавляем слушателя мыши на панель
+                    public void mouseClicked(MouseEvent e) {
+                        add(e);
+                    }
+                });
+                frame.setVisible(true);
+            }
+        });
     }
 }
